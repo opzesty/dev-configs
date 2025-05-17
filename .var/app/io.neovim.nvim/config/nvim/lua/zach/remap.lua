33ca6d61f04d2d1df0ae1f27a6339b953a44917e
Zach Ramirez <stzu06rel@gmail.com>
2025-05-17 16:06:17 -0400

    create remap to vimgrep for word under cursor and copen the results

--- .var/app/io.neovim.nvim/config/nvim/lua/zach/remap.lua
+++ .var/app/io.neovim.nvim/config/nvim/lua/zach/remap.lua
@@ -1,1 +1,7 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "<leader>pp", function()
+    local word = vim.fn.expand("<cword>")
+    vim.cmd("vimgrep /" .. word .. "/ **")
+    vim.cmd("copen")
+end, { desc = "Vimgrep for word under cursor" })
