d1e200bf73b341eb02e72d3449ad07354077e19b
Zach Ramirez <stzu06rel@gmail.com>
2025-05-24 16:23:00 -0400

    qol improvement to aware.end_activity

--- .var/app/io.neovim.nvim/config/nvim/lua/zach/aware.lua
+++ .var/app/io.neovim.nvim/config/nvim/lua/zach/aware.lua
@@ -4,7 +4,7 @@ local M = {}
 function M.end_activity(current_line)
 
 	local now = os.date("%H:%M\t%d %b %Y\t")
-	vim.fn.append(vim.fn.line("$"), now)
+	vim.fn.append(vim.fn.line("$"), now .. "	duration=;")
 	local row = vim.fn.line(".")
 	local updated = current_line:gsub("duration=;", "duration=" .. duration.duration_diff(current_line, now) .. ";")
 	vim.fn.setline(row, updated)
